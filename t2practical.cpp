8e328f3fcbb9dd7570e56c6d237f8529c2d8235f
Akash Yadav <91262354+akash701752@users.noreply.github.com>
2021-10-06 10:19:39 +0530

    Create t2practical.cpp

---
+++ t2practical.cpp
@@ -0,0 +1,54 @@
+
+#include<bits/stdc++.h>
+#include<iostream>
+#include<string>
+using namespace std ;
+
+int main() //Task  2
+{
+   // Task 2 to Check a string is Palindrome or not
+  string str1   , str2 ;
+  cout<<"Enter  String 1     : ";
+  cin>> str1 ;
+  //cout<<"Enter  String 2     : ";
+  //cin>> str2 ;
+  int i  , j , k = str1.length(), count = 0 ;
+  cout<< "String Length is :  " << k << endl  ;
+  //if((str1.length())==(str2.length()))
+  // {
+    for(i=0;i<=k;i++)
+    {
+      /*for(j=k ; j>=0 ;j--)
+      { 
+        if(str1[i]==str[j])
+        {
+          count ++ ;
+        }
+        /*else
+        {
+          break ;
+        } 
+      } */
+      if(str1[i]==str1[k])
+      {
+        count ++ ;
+        k -- ;
+      }
+    }
+    if(count==str1.size())
+    {
+      cout<< "String is Palindrome !! " << endl ;
+    }
+    else
+    {
+      cout<< "String is NOT Palindrome !! " << endl ;
+    }
+ //}
+ /*
+  else
+  {
+    cout<< "Both Strings are NOT Palindrome !! " << endl ;
+  }
+  */
+ return 0 ;
+}

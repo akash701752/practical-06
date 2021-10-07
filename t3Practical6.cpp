8a70dca2ca2093b753e898ab30386c2578465b46
Akash Yadav <91262354+akash701752@users.noreply.github.com>
2021-10-07 09:43:47 +0530

    Update t3Practical6.cpp

--- t3Practical6.cpp
+++ t3Practical6.cpp
@@ -1,1 +1,25 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<string>
+#define MAX 20
+using namespace std ;
+
+int main() //Task  2
+{
+   // Task 3 to Reverse a String  
+  string str1 ;
+  cout<<"Enter a String : " ;
+  getline(cin,str1);
+  string str2 ;
+  string :: reverse_iterator rit;
+  for(rit=str1.rbegin();rit!=str1.rend();++rit)
+  {
+    str2 = str2 + *rit ;
+  }
+  cout<<"Reverse String is : "<< str2 << endl ;
+  //cout<<str1.size() << endl ;
+  //cout<<str2.size() << endl ;
+  
+ return 0 ;
+}
 
